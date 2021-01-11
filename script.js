adcd143b91982ad18c06a49135ee943b010805c3
anderooc <andrew@Andrews-MacBook-Air.local>
2021-01-11 10:57:41 -0800

    Fixed and final.

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ function myClick () {
   var lname = document.getElementById('lname').value;
   console.log(fname, lname);
   nameQuestion.innerHTML = "\n";
-  nameQuestion.innerHTML += "\t\t<h1>Hello <span id="fname"><span id="lname"></h1>\n"
-  // Notice mixing of quotation marks, just like in Python.
-  nameQuestion.innerHTML += "\t\t<img src='default.png' />\n";
-  // Notice here that we are appending the values of the variables.
+  nameQuestion.innerHTML += "\t\t<p>" + "Hello" + "</p>\n";
   nameQuestion.innerHTML += "\t\t<p>" + fname + " " + lname + "</p>\n";
 }
