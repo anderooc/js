acaa6e64ff0775c85185deb23ed9e917ec3334af
anderooc <andrew@Andrews-MacBook-Air.local>
2021-01-20 11:03:03 -0800

    Added radio code, added in local storage and JSON, slight fixes as well.

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 nameQuestion = document.getElementById("nameQuestion");
 transportation = document.getElementById("transportation");
-vehicle1 = document.getElementById("vehicle1");
-vehicle2 = document.getElementById("vehicle2");
-vehicle3 = document.getElementById("vehicle3");
-
+grade = document.getElementById("gradeQuestion")
+var myJSON = {}
 function nameSubmit () {
   var fname = document.getElementById('fname').value;
   var lname = document.getElementById('lname').value;
-  console.log(fname, lname);
+  myJSON = Object.assign({"fname": fname, "lname":lname}, myJSON);
+  console.log(myJSON);
+  console.log(JSON.stringify(myJSON));
+  console.log(JSON.parse(JSON.stringify(myJSON)));
   nameQuestion.innerHTML = "\n";
   nameQuestion.innerHTML += "\t\t<p>" + "Hello " + fname + " " + lname + "</p>";
 }
 
 function checkboxSubmit () {
-  var vehicle = document.querySelector('.checkbox')
-  if (vehicle1.checked) {
-    transportation.innerHTML = "\n";
+  var vehicle = document.querySelector('vehicle').checked;
+  myJSON = Object.assign({"vehicle": vehicle}, myJSON);
+  console.log(myJSON)
+  console.log(JSON.stringify(myJSON));
+  console.log(JSON.parse(JSON.stringify(myJSON)));
+  transportation.innerHTML = "\n";
+  if (vehicle == "Bike") {
     transportation.innerHTML += "\t\t<p>" + "You take your bike to school." + "</p>";
   }
-  else if (vehicle2.checked) {
-    transportation.innerHTML = "\n";
+  else if (vehicle == "Bus") {
     transportation.innerHTML += "\t\t<p>" + "You take a bus to school." + "</p>";
   }
-  else if (vehicle3.checked) {
-    transportation.innerHTML = "\n";
+  else if (vehicle == "Car") {
     transportation.innerHTML += "\t\t<p>" + "You take a car to school." + "</p>";
   }
   else {
-    transportation.innerHTML = "\n";
     transportation.innerHTML += "\t\t<p>" + "You don't take transportation to school." + "</p>";
   }
+}
+
+function gradeSubmit () {
+  var gradeLevel = document.querySelector('input[name="grade"]:checked').value;
+  myJSON = Object.assign({"gradeLevel": gradeLevel}, myJSON);
+  console.log(myJSON);
+  console.log(JSON.stringify(myJSON));
+  console.log(JSON.parse(JSON.stringify(myJSON)));
+  grade.innerHTML = "\n";
+  grade.innerHTML += "\t\t<p>" + "You are in " + gradeLevel + " grade." + "</p>";
 }
