f926ca225034d92fd5580ed6830058e786f53ac9
anderooc <andrew.chang.22@bishops.com>
2021-01-28 10:57:27 -0800

    Local Storage for all buttons

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 nameQuestion = document.getElementById("nameQuestion");
 transportation = document.getElementById("transportation");
-grade = document.getElementById("gradeQuestion")
-var myJSON = {}
+grade = document.getElementById("gradeQuestion");
+
+var nameJSON = {}
+var vehicleJSON = {}
+var gradeJSON = {} // should i just make one json
+
 function nameSubmit () {
   var fname = document.getElementById('fname').value;
   var lname = document.getElementById('lname').value;
-  myJSON = Object.assign({"fname": fname, "lname":lname}, myJSON);
-  console.log(myJSON);
-  console.log(JSON.stringify(myJSON));
-  console.log(JSON.parse(JSON.stringify(myJSON)));
+
+  nameJSON = Object.assign({'fname': fname, 'lname':lname}, nameJSON);
+  console.log(nameJSON);
+  localStorage.setItem('fname', JSON.stringify(fname));
+  localStorage.setItem('lname', JSON.stringify(lname));
+  var fnameRetrieved = localStorage.getItem('fname')
+  var lnameRetrieved = localStorage.getItem('lname')
+
   nameQuestion.innerHTML = "\n";
-  nameQuestion.innerHTML += "\t\t<p>" + "Hello " + fname + " " + lname + "</p>";
+  nameQuestion.innerHTML += "\t\t<p>" + "Hello " + fnameRetrieved + " " + lname + "</p>";
 }
 
-function checkboxSubmit () {
-  var vehicle = document.querySelector('vehicle').checked;
-  myJSON = Object.assign({"vehicle": vehicle}, myJSON);
-  console.log(myJSON)
-  console.log(JSON.stringify(myJSON));
-  console.log(JSON.parse(JSON.stringify(myJSON)));
+function transportationSubmit () {
+  var vehicle = document.getElementById('vehicle').checked;
+
+  vehicleJSON = Object.assign({"vehicle": vehicle}, vehicleJSON);
+  console.log(vehicleJSON);
+  console.log(JSON.stringify(vehicleJSON));
+  localStorage.setItem('vehicle', JSON.stringify(vehicle));
+
   transportation.innerHTML = "\n";
-  if (vehicle == "Bike") {
+  if (vehicleJSON["vehicle"] == "Bike") {
     transportation.innerHTML += "\t\t<p>" + "You take your bike to school." + "</p>";
   }
-  else if (vehicle == "Bus") {
+  else if (vehicleJSON["vehicle"] == "Bus") {
     transportation.innerHTML += "\t\t<p>" + "You take a bus to school." + "</p>";
   }
-  else if (vehicle == "Car") {
+  else if (vehicleJSON["vehicle"] == "Car") {
     transportation.innerHTML += "\t\t<p>" + "You take a car to school." + "</p>";
   }
   else {
@@ -36,10 +46,12 @@ function checkboxSubmit () {
 
 function gradeSubmit () {
   var gradeLevel = document.querySelector('input[name="grade"]:checked').value;
-  myJSON = Object.assign({"gradeLevel": gradeLevel}, myJSON);
-  console.log(myJSON);
-  console.log(JSON.stringify(myJSON));
-  console.log(JSON.parse(JSON.stringify(myJSON)));
+
+  gradeJSON = Object.assign({"gradeLevel": gradeLevel}, gradeJSON);
+  console.log(gradeJSON);
+  console.log(JSON.stringify(gradeJSON));
+  localStorage.setItem('gradeLevel', JSON.stringify(gradeLevel));
+
   grade.innerHTML = "\n";
   grade.innerHTML += "\t\t<p>" + "You are in " + gradeLevel + " grade." + "</p>";
 }
