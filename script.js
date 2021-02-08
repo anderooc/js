653040e83ac08526bd731fb21d4f89624de2f680
anderooc <andrew.chang.22@bishops.com>
2021-02-07 23:51:19 -0800

    Simplified to only 1 div, added a pie chart div

--- script.js
+++ script.js
@@ -1,57 +1,46 @@
-nameQuestion = document.getElementById("nameQuestion");
-transportation = document.getElementById("transportation");
-grade = document.getElementById("gradeQuestion");
+mainSurvey = document.getElementById("survey");
+pieChart = document.getElementById("pieChart");
 
-var nameJSON = {}
-var vehicleJSON = {}
-var gradeJSON = {} // should i just make one json
-
-function nameSubmit () {
+function submitSurvey() {
+  // Function that runs after clicking submit button
+  // Gets name, grade level, and vehicles, then puts them in JSON
   var fname = document.getElementById('fname').value;
   var lname = document.getElementById('lname').value;
 
-  nameJSON = Object.assign({'fname': fname, 'lname':lname}, nameJSON);
-  console.log(nameJSON);
-  localStorage.setItem('fname', JSON.stringify(fname));
-  localStorage.setItem('lname', JSON.stringify(lname));
-  var fnameRetrieved = localStorage.getItem('fname')
-  var lnameRetrieved = localStorage.getItem('lname')
+  var gradeLevel = document.querySelector('input[name="grade"]:checked').value;
 
-  nameQuestion.innerHTML = "\n";
-  nameQuestion.innerHTML += "\t\t<p>" + "Hello " + fnameRetrieved + " " + lname + "</p>";
+  var vehicles = document.querySelectorAll('input[name="' + vehicle + '"]:checked'),values=[];
+  Array.prototype.forEach.call(vehicles, function(el) {
+        vehicles.push(el.value);
+    });
+
+  myJSON = Object.assign({
+    'fname': fname,
+    'lname':lname,
+    'gradeLevel':gradeLevel,
+    'vehicles':vehicles
+  }, myJSON);
+  console.log(myJSON);
+
+
 }
 
-function transportationSubmit () {
-  var vehicle = document.getElementById('vehicle').checked;
 
-  vehicleJSON = Object.assign({"vehicle": vehicle}, vehicleJSON);
-  console.log(vehicleJSON);
-  console.log(JSON.stringify(vehicleJSON));
-  localStorage.setItem('vehicle', JSON.stringify(vehicle));
+/* if (nameCheck == 1 && vehicleCheck == 1 && gradeCheck == 1){
+  var vehicleRetrieved = localStorage.getItem('vehicle');
+  var gradeRetrieved = localStorage.getItem('gradeLevel');
 
-  transportation.innerHTML = "\n";
-  if (vehicleJSON["vehicle"] == "Bike") {
+  if (vehicleRetrieved == "Bike") {
     transportation.innerHTML += "\t\t<p>" + "You take your bike to school." + "</p>";
   }
-  else if (vehicleJSON["vehicle"] == "Bus") {
+  else if (vehicleRetrieved == "Bus") {
     transportation.innerHTML += "\t\t<p>" + "You take a bus to school." + "</p>";
   }
-  else if (vehicleJSON["vehicle"] == "Car") {
+  else if (vehicleRetrieved == "Car") {
     transportation.innerHTML += "\t\t<p>" + "You take a car to school." + "</p>";
   }
   else {
     transportation.innerHTML += "\t\t<p>" + "You don't take transportation to school." + "</p>";
   }
-}
-
-function gradeSubmit () {
-  var gradeLevel = document.querySelector('input[name="grade"]:checked').value;
-
-  gradeJSON = Object.assign({"gradeLevel": gradeLevel}, gradeJSON);
-  console.log(gradeJSON);
-  console.log(JSON.stringify(gradeJSON));
-  localStorage.setItem('gradeLevel', JSON.stringify(gradeLevel));
-
-  grade.innerHTML = "\n";
   grade.innerHTML += "\t\t<p>" + "You are in " + gradeLevel + " grade." + "</p>";
-}
+} */
